2d4be7aac3fa21e899c72abb70e2314ce4c8d08a
Akansha Saxena <akansha.saxena0211@gmail.com>
2022-12-11 01:15:03 +0530

    CategoryController Implemented

---
+++ src/main/java/com/akansha/app/blog/controllers/CategoryConroller.java
@@ -0,0 +1,72 @@
+package com.akansha.app.blog.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.akansha.app.blog.payloads.ApiResponse;
+import com.akansha.app.blog.payloads.CategoryDto;
+import com.akansha.app.blog.services.CategoryService;
+
+@RestController
+@RequestMapping("/apis/categories")
+public class CategoryConroller {
+	// Controller --> exposes our resources through an endpoint (deals with coming request)
+	// LAYERED ARCHITECTURE
+	
+	// created CategoryRepo 
+	// write Interface for all operations you want on Repo(i.e Service)
+	// implement all interface methods (CategoryServiceImpl)
+	// can use the object (autowired) of serviceImpl inside Controller
+	
+	// created Catregory Entity and CategoryDto 
+	// created CtaegoryRepositries for db operations
+	// category service
+	// categoryService impl
+	// all apis exposed using CategoryController
+	
+	private CategoryService categoryService;
+	// create
+	@PostMapping("/")
+	public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto){
+		CategoryDto createCategory = this.categoryService.createCategory(categoryDto);
+		return new ResponseEntity<CategoryDto>(createCategory, HttpStatus.CREATED);
+	}
+	
+	// update
+	@PutMapping("/{categoryId}")
+	public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto, @PathVariable Integer categoryId){
+		CategoryDto updatedCategory = this.categoryService.updateCategory(categoryDto, categoryId);
+		return new ResponseEntity<CategoryDto>(updatedCategory, HttpStatus.OK);
+	}
+	
+	// delete
+	@DeleteMapping("/{categoryId}")
+	public ResponseEntity<ApiResponse> deleteCategory( @PathVariable Integer categoryId){
+		this.categoryService.deleteCategory(categoryId);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("Category is deleted successfully !!", true), HttpStatus.OK);
+	}
+	
+	// get
+	@GetMapping("/{categoryId}")
+	public ResponseEntity<CategoryDto> getCategory( @PathVariable Integer categoryId){
+		CategoryDto categoryDto = this.categoryService.getCategory(categoryId);
+		return new ResponseEntity<CategoryDto>(categoryDto, HttpStatus.OK);
+	}
+	
+	// getAll
+	@GetMapping("/")
+	public ResponseEntity<List<CategoryDto>> getCategories( @PathVariable Integer categoryId){
+		List<CategoryDto> categories = this.categoryService.getCategories();
+		return ResponseEntity.ok(categories);
+	}
+}
--- src/main/java/com/akansha/app/blog/services/CategoryServiceImpl.java
+++ src/main/java/com/akansha/app/blog/services/CategoryServiceImpl.java
@@ -1,42 +1,72 @@
 package com.akansha.app.blog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.akansha.app.blog.entities.Category;
+import com.akansha.app.blog.exceptions.ResourceNotFoundException;
 import com.akansha.app.blog.payloads.CategoryDto;
+import com.akansha.app.blog.repositories.CategoryRepo;
 
 public class CategoryServiceImpl implements CategoryService{
 
 	 // interface is for loose coupling , How ?? 
 	
+	@Autowired
+	private CategoryRepo categoryRepo;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	@Override
 	public CategoryDto createCategory(CategoryDto categoryDto) {
 		// TODO Auto-generated method stub
-		return null;
+		Category category = this.modelMapper.map(categoryDto, Category.class);
+		Category addedCategory =  this.categoryRepo.save(category);
+		return this.modelMapper.map(addedCategory, CategoryDto.class);
 	}
 
 	@Override
 	public CategoryDto updateCategory(CategoryDto categoryDto, Integer categoryId) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		Category category = this.categoryRepo.findById(categoryId)
+				.orElseThrow(()-> new ResourceNotFoundException("Category", "Category id", categoryId));
+		category.setCategoryTitle(categoryDto.getCategoryTitle());
+		category.setCategoryDescription(categoryDto.getCategoryDescription());
+		Category updatedCategory = this.categoryRepo.save(category);
+		return this.modelMapper.map(updatedCategory, CategoryDto.class);
 	}
-
+	
 	@Override
 	public void deleteCategory(Integer categoryId) {
 		// TODO Auto-generated method stub
+		
+		Category category = this.categoryRepo.findById(categoryId)
+				.orElseThrow(()-> new ResourceNotFoundException("Category", "categoryId", categoryId));
+		this.categoryRepo.delete(category);
 		
 	}
 
 	@Override
 	public CategoryDto getCategory(Integer categoryId) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		Category category = this.categoryRepo.findById(categoryId)
+				.orElseThrow(()-> new ResourceNotFoundException("Category", "categoryId", categoryId));
+		
+		return this.modelMapper.map(category, CategoryDto.class);
 	}
 
 	@Override
 	public List<CategoryDto> getCategories() {
 		// TODO Auto-generated method stub
-		return null;
+		List<Category> categories= this.categoryRepo.findAll();
+		List<CategoryDto> categoryDtos = categories.stream().map((category)-> this.modelMapper.map(categories, CategoryDto.class)).collect(Collectors.toList());
+		return categoryDtos;
 	}
 	
 	
