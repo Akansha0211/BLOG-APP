00c6808273ae90fca5c08dc7725ddc909165ca2d
Akansha Saxena <akansha.saxena0211@gmail.com>
2022-12-11 13:32:17 +0530

    Added relationship between category and post & user & post tables

--- src/main/java/com/akansha/app/blog/entities/Post.java
+++ src/main/java/com/akansha/app/blog/entities/Post.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,5 +25,11 @@ public class Post {
 	private String imageName;
 	
 	private Date addedDate;
+	
+	@ManyToOne
+	private Category category;
+	
+	@ManyToOne
+	private User user;
 	
 }
--- src/main/java/com/akansha/app/blog/entities/User.java
+++ src/main/java/com/akansha/app/blog/entities/User.java
@@ -1,10 +1,15 @@
 package com.akansha.app.blog.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,4 +29,7 @@ public class User {
 	private String email;
 	private String password;
 	private String about;
+	
+	@OneToMany(mappedBy="user", cascade = CascadeType.ALL)
+	private List<Post> posts = new ArrayList<>();
 }
