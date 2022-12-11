185a11601873cf5f26438b369e927b1549442f6e
Akansha Saxena <akansha.saxena0211@gmail.com>
2022-12-11 14:37:45 +0530

    Add all unimplemented methods of PostService in its implementation class

---
+++ src/main/java/com/akansha/app/blog/services/PostServiceImpl.java
@@ -0,0 +1,58 @@
+package com.akansha.app.blog.services;
+
+import java.util.List;
+
+import com.akansha.app.blog.entities.Post;
+import com.akansha.app.blog.payloads.PostDto;
+
+public class PostServiceImpl implements PostService{
+
+	@Override
+	public Post createPost(PostDto postDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Post updatePost(PostDto postDto, Integer postId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletePost(Integer postId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Post> getAllPost() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Post getPostById(Integer postId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Post> getPostsByCategory(Integer categoryId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Post> getPostsByUser(Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Post> searchPosts(String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
